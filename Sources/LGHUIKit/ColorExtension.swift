5954431710b8a33d5c976f4dacb9076dcc4c1511
liangguanghui <liangguanghui@keep.com>
2022-01-02 02:03:00 +0800

    feature: 环境测试

---
+++ Sources/LGHUIKit/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by 梁光辉 on 2022/1/2.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+extension Color {
+    public static let gh_blue: Color = Color(UIColor.gh_blue())
+}
---
+++ Sources/LGHUIKit/UIColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by 梁光辉 on 2022/1/2.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    public static func gh_blue() -> UIColor {
+        return UIColor.blue
+    }
+}
